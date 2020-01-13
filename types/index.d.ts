42be0af2fcb7389a46b5044e54305cf470dd0542
SHEN <>
2020-01-13 10:04:56 +0800

    dev

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,27 @@
 declare module "ss_react_ts_ui" {
+  export import SButton = ElementReact.Button
+}
 
+declare namespace SSReactLibs {
+  interface ComponentProps<T> {
+    className?: string;
+    style?: React.CSSProperties;
+  }
+  class Component<P, S> extends React.Component<P, S> {
+    classNames?(...args): any
+    className?(...args): any
+    style?(agrs?): any
+  }
+}
 
+declare namespace SSReactUI {
+  // SButton
+  interface SButtonProps extends SSReactLibs.ComponentProps<{}> {
+    onClick?(e: React.SyntheticEvent<HTMLButtonElement>): void;
+    color?: string;
+    bgColor?: string;
+    title?: string;
+  }
+  export class SButton extends SSReactLibs.Component<SButtonProps, {}> {
+  }
 }
